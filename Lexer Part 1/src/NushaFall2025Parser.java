8aae2a9ed1188580655ba56abdf9a8c7c553333a
Mamadou325 <mamadouwague805@gmail.com>
2026-02-04 21:41:39 -0500

    Add files via upload

---
+++ Lexer Part 1/src/NushaFall2025Parser.java
@@ -0,0 +1,13 @@
+import AST.*;
+import java.util.Optional;
+import java.util.LinkedList;
+
+public class NushaFall2025Parser {
+    public NushaFall2025Parser() {
+
+    }
+
+    public Optional<Nusha> Nusha(LinkedList<Token> tokens) throws SyntaxErrorException {
+
+    }
+}
